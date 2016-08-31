2e7de1cde01a9a8712f374f6c70ee55be79b19c8
adambar <adam.bar@brightinventions.pl>
2016-08-31 13:30:34 +0200

    Responsive menu; font

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 require("normalize.css/normalize.css");
 require("./style.scss");
+
+document.getElementById('nav-icon').addEventListener('click', event => document.getElementsByTagName('nav')[0].classList.toggle('expanded'));
