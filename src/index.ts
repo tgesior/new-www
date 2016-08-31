20c27f2a8efc1d20ac80ffbf3a5d14f438d06e03
adambar <adam.bar@brightinventions.pl>
2016-08-31 14:28:23 +0200

    Fix webpack config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-require("normalize.css/normalize.css");
-require("./style.scss");
+require('normalize.css/normalize.css');
+require('./style.scss');
+require('file?name=[name].[ext]!./index.html');
 
 document.getElementById('nav-icon').addEventListener('click', event => document.getElementsByTagName('nav')[0].classList.toggle('expanded'));
