a7dca8688ddc2225ccdf4ac400debab9aa3d1e02
adambar <adam.bar@brightinventions.pl>
2016-09-15 07:31:52 +0200

    More CSS prefixes

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
     ],
     postcss: [
         autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: ['>1%', 'last 2 versions']
         })
     ]
 };
